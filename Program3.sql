03d0f4178c31becdd3450294dee6274289239102
Aravindh R Shankar <60482854+AravR-bms@users.noreply.github.com>
2020-05-13 09:02:56 +0530

    Update

--- Program3.sql
+++ Program3.sql
@@ -1,68 +1,68 @@
-create database SupplierDB;
-use SupplierDB;
-create table Supplier(
-sid int,
-sname varchar(30),
-city varchar(30),
-primary key(sid));
-desc Supplier;
-create table Parts(
-pid int,
-pname varchar(30),
-color varchar(30),
-primary key(pid));
-create table Catalog(
-sid int,
-pid int,
-cost int,
-primary key(sid,pid),
-foreign key (sid) references Supplier(sid),
-foreign key (pid) references Parts(pid));
-insert into Supplier values(10001,'Acme Widget','Bangalore'),(10002,'Johns','Kolkata'),(10003,'Vimal','Mumbai'),(10004,'Reliance','Delhi');
-insert into Parts values(20001,'Book','Red'),(20002,'Pen','Red'),(20003,'pencil','Green'),(20004,'Mobile','Green'),(20005,'Charger','Black');
-insert into Catalog values(10001,20001,10),(10001,20002,10),(10001,20003,30),(10001,20004,10),(10001,20005,10),(10002,20001,10),(10002,20002,20),(10003,20003,30),(10004,20003,40);
-select * from Supplier;
-select * from Parts;
-select * from Catalog;
-
-select distinct pname 
-from Parts, Catalog
-where Parts.pid=Catalog.pid;
-
-select sname 
-from Supplier
-where not exists( select pid from Parts
-					except
-                    select distinct pid 
-                    from Catalog
-                    where Catalog.sid=Supplier.sid);
-
-select sname 
-from Supplier
-where not exists( select pid from Parts where color='Red'
-					except
-                    select distinct Catalog.pid 
-                    from Catalog , Parts
-                    where Parts.pid=Catalog.pid and Parts.color='Red' and  Catalog.sid=Supplier.sid);
-                    
-
-select pname
-from Parts,Catalog,Supplier
-where Catalog.pid=Parts.pid and Catalog.sid=Supplier.sid 
-and Supplier.sname='Acme Widget' and Catalog.pid not in (select c.pid from Catalog c ,Supplier s
-							where s.sid=c.sid and s.sname<>'Acme Widget');
+CREATE DATABASE supplierDB;
+USE supplierDB;
 
 
-select sid
-from Catalog c
-where c.cost>( select avg(c1.cost)
-		from Catalog c1
-		where c.pid=c1.pid);
-                
-                
-select p.pid, s.sname
-from Parts p, Supplier s, Catalog c
-where p.pid=c.pid and s.sid=c.sid
-and c.cost=(select max(c1.cost)
-	    from Catalog c1
-            where c1.pid=c.pid);
+CREATE TABLE SUPPLIERS (
+    sid INT,
+    sname VARCHAR(30),
+    city VARCHAR(30),
+    PRIMARY KEY (sid)
+);
+
+CREATE TABLE PARTS (
+    pid INT,
+    pname VARCHAR(30),
+    color VARCHAR(30),
+    PRIMARY KEY (pid)
+);
+
+CREATE TABLE CATALOG (
+    sid INT,
+    pid INT,
+    cost INT,
+    PRIMARY KEY (sid , pid),
+    FOREIGN KEY (sid)
+        REFERENCES SUPPLIERS (sid),
+    FOREIGN KEY (pid)
+        REFERENCES PARTS (pid)
+);
+
+INSERT INTO SUPPLIERS VALUES(10001,'Acme Widget','Bangalore'),(10002,'Johns','Kolkata'),(10003,'Vimal','Mumbai'),(10004,'Reliance','Delhi');
+INSERT INTO PARTS VALUES(20001,'Book','Red'),(20002,'Pen','Red'),(20003,'pencil','Green'),(20004,'Mobile','Green'),(20005,'Charger','Black');
+INSERT INTO CATALOG VALUES(10001,20001,10),(10001,20002,10),(10001,20003,30),(10001,20004,10),(10001,20005,10),(10002,20001,10),(10002,20002,20),(10003,20003,30),(10004,20003,40);
+
+
+-- QUESTIONS:
+
+-- 1
+SELECT pname
+FROM PARTS P, CATALOG C
+WHERE P.pid=C.pid;
+
+-- 2
+SELECT S.sname
+FROM SUPPLIERS S
+WHERE NOT EXISTS (SELECT P.pid FROM PARTS P WHERE P.pid
+		  NOT IN (SELECT DISTINCT C.pid FROM CATALOG C WHERE C.sid=S.sid));
+
+-- 3
+SELECT sname 
+FROM SUPPLIERS S
+WHERE NOT EXISTS (SELECT pid FROM PARTS WHERE color='Red'
+		  NOT IN (SELECT DISTINCT C.pid 
+			  FROM CATALOG C, PARTS P
+			  WHERE P.pid=C.pid AND P.color='Red' AND C.sid=S.sid));
+
+-- 4
+SELECT pname
+FROM PARTS P, CATALOG C, SUPPLIERS S
+WHERE C.pid=P.pid AND C.sid=S.sid 
+AND S.sname='Acme Widget' AND C.pid 
+	NOT IN (SELECT c.pid 
+		FROM CATALOG C, SUPPLIERS S
+		WHERE S.sid=C.sid AND S.sname<>'Acme Widget');
+                            
+-- 5
+SELECT sid
+FROM CATALOG C
+WHERE C.cost>(SELECT AVG(C2.cost) FROM CATALOG C2 WHERE C.pid=C2.pid);
